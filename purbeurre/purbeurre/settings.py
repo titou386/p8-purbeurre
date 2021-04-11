0df2d92970c0d219438df93142a95ca7b824b704
Yves <yves.dornier@yahoo.fr>
2021-04-11 21:37:34 +0200

    models defined

--- purbeurre/purbeurre/settings.py
+++ purbeurre/purbeurre/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'search.apps.SearchConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
-    'search',
 ]
 
 MIDDLEWARE = [
--- purbeurre/search/models.py
+++ purbeurre/search/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    code = models.BigIntegerField(unique=True)
+    name = models.CharField(max_length=100)
+    image_url = models.CharField(max_length=150)
+
+    quantity = models.CharField(max_length=100)
+    nutriscore = models.CharField(max_length=1)
+
+    energy_kcal_100g = models.DecimalField(max_digits=6, decimal_places=2)
+    fat_100g = models.DecimalField(max_digits=5, decimal_places=2)
+    saturated_fat_100g = models.DecimalField(max_digits=5, decimal_places=2)
+    carbohydrates_100g = models.DecimalField(max_digits=5, decimal_places=2)
+    sugars_100g = models.DecimalField(max_digits=5, decimal_places=2)
+    fiber_100g = models.DecimalField(max_digits=5, decimal_places=2)
+    proteins_100g = models.DecimalField(max_digits=5, decimal_places=2)
+    salt_100g = models.DecimalField(max_digits=5, decimal_places=2)
+
+    energy_kcal_100g_unit = models.CharField(max_length=10)
+    fat_100g_unit = models.CharField(max_length=10)
+    saturated_fat_100g_unit = models.CharField(max_length=10)
+    carbohydrates_100g_unit = models.CharField(max_length=10)
+    sugars_100g_unit = models.CharField(max_length=10)
+    fiber_100g_unit = models.CharField(max_length=10)
+    proteins_100g_unit = models.CharField(max_length=10)
+    salt_100g_unit = models.CharField(max_length=10)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+
+
+class ProductCategory(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['product', 'category'], name="product-categorised")
+        ]
