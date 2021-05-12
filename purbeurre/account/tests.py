bb15bf574148572b34659c125d0df5dc3fff76cf
Yves <yves.dornier@yahoo.fr>
2021-05-12 22:25:37 +0200

    Tests

--- purbeurre/account/tests.py
+++ purbeurre/account/tests.py
@@ -1,3 +1,109 @@
 from django.test import TestCase
+from selenium import webdriver
+from account.models import User
+from search.models import Product
 
-# Create your tests here.
+
+class RegisterPageTestCase(TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def test_01_register_firefox(self):
+        self.driver.get("http://localhost:8000/account/register/")
+        self.driver.find_element_by_id('id_username').send_keys('Test')
+        self.driver.find_element_by_id('id_email').send_keys('test@test.fr')
+        self.driver.find_element_by_id('id_password1').send_keys('1234')
+        self.driver.find_element_by_id('id_password2').send_keys('1234')
+        self.driver.find_element_by_name('submit').click()
+        self.assertIn("http://localhost:8000/account/",
+                      self.driver.current_url)
+
+    def test_02_login_firefox(self):
+        self.driver.get("http://localhost:8000/account/")
+        self.driver.find_element_by_id('id_username').send_keys('test@test.fr')
+        self.driver.find_element_by_id('id_password').send_keys('1234')
+        self.assertEquals("http://localhost:8000/account/",
+                          self.driver.current_url)
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+class RegisterViewTestCase(TestCase):
+
+    def setUp(self):
+        u = User.objects.create(username='Pauline', email='pauline@free.fr')
+        u.set_password('1234')
+        u.save()
+
+    def test_register_success(self):
+        response = self.client.post('/account/register/',
+                                    {'username': "Jacques",
+                                     'email': "jacques@free.fr",
+                                     'password1': '1234',
+                                     'password2': '1234'})
+
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(response.url, "/account/")
+
+    def test_register_email_fail(self):
+        response = self.client.post('/account/register/',
+                                    {'username': "Paulinette",
+                                     'email': "pauline@free.fr",
+                                     'password1': '2345',
+                                     'password2': '2345'})
+
+        self.assertContains(response, "L'email est déjà enregistré", html=True)
+
+    def test_register_mismatch_password(self):
+        response = self.client.post('/account/register/',
+                                    {'username': "Vincent",
+                                     'email': "vincent@free.fr",
+                                     'password1': '1234',
+                                     'password2': '2345'})
+        self.assertContains(response, "Le mot de passe ne correspond pas",
+                            html=True)
+
+    def test_no_register_if_logged(self):
+        self.client.post('/account/',
+                         {'username': 'pauline@free.fr',
+                          'password': '1234'})
+        response = self.client.get('/account/register/')
+        self.assertEquals(response.status_code, 302)
+
+
+class LoginViewTestCase(TestCase):
+
+    def setUp(self):
+        u = User.objects.create(username='Michelle', email='michelle@free.fr')
+        u.set_password('1234')
+        u.save()
+
+    def test_login_success(self):
+
+        response = self.client.login(username='michelle@free.fr',
+                                     password='1234')
+        self.assertTrue(response)
+
+
+class SaveAndMySubstitionsViewTestCase(TestCase):
+
+    def setUp(self):
+        u = User.objects.create(username='Pauline', email='pauline@free.fr')
+        u.set_password('1234')
+        u.save()
+        self.p = Product.objects.create(name="Nutella 650g",
+                                        code="758511125439",
+                                        image_url="https://nut.fr/prod.jpg",
+                                        nutriscore='e')
+
+    def test_save_substitution(self):
+
+        self.client.post('/account/',
+                         {'username': 'pauline@free.fr',
+                          'password': '1234'})
+        response = self.client\
+            .post(f'/account/substitutions/{self.p.id}/save/')
+        response = self.client.get(response.url)
+        self.assertContains(response, "Nutella 650g", html=True)
--- purbeurre/search/tests.py
+++ purbeurre/search/tests.py
@@ -1,3 +1,59 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from search.models import Product, Category
+
+
+class IndexPageTestCase(TestCase):
+    def test_index_page(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+
+class ResultsViewTestCase(TestCase):
+    def setUp(self):
+        Product.objects.create(name="Pâte à tartiner aux noisettes",
+                               code="758511125439",
+                               image_url="https://nutella.com/product.jpg",
+                               nutriscore='e')
+        Product.objects.create(name="Chocolat aux noisettes",
+                               code="758516525439",
+                               image_url="https://milka.com/product.jpg",
+                               nutriscore='d')
+
+    def test_result_returns_1_product(self):
+        response = self.client.get(reverse('search'), {'query': 'tartiner'})
+        self.assertEqual(len(response.context_data['results']), 1)
+
+    def test_result_returns_2_products(self):
+        response = self.client.get(reverse('search'), {'query': 'noisettes'})
+        self.assertEqual(len(response.context_data['results']), 2)
+
+    def test_result_returns_0_product(self):
+        response = self.client.get(reverse('search'), {'query': 'confiture'})
+        self.assertEqual(len(response.context_data['results']), 0)
+
+
+class SubstitutionViewTestCase(TestCase):
+    def setUp(self):
+        p1 = Product.objects.create(name="Nutella 650g",
+                                    code="758511125439",
+                                    image_url="https://nutella.fr/product.jpg",
+                                    nutriscore='e')
+        p2 = Product.objects.create(name="Pâte à tartiner aux noisettes",
+                                    code="5469725548545",
+                                    image_url="https://choco.com/product.jpg",
+                                    nutriscore='d')
+        c = Category.objects.create(name='Pâte à tartiner')
+        c.products.add(p1)
+        c.products.add(p2)
+
+    def test_substitution_returns_1_product(self):
+        p = Product.objects.get(code="758511125439")
+        response = self.client.get(f'/product/{p.id}/substitutions/')
+        self.assertEqual(len(response.context_data['substitutions']), 1)
+
+    def test_substitutions_returns_0_product(self):
+        p = Product.objects.get(code="5469725548545")
+        response = self.client.get(f'/product/{p.id}/substitutions/')
+        self.assertEqual(len(response.context_data['substitutions']), 0)
